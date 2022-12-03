d3098ba7951004355dbea02d7a2c1a16f813128d
Espen MH <espen@monopoint.net>
2022-12-03 14:13:03 +0100

    cleanup

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -5,24 +5,31 @@ use std::{char, fs};
 
 #[allow(unused)]
 fn main() {
-    let file = fs::read_to_string("./input").expect("no file");
-    let part1_input = file.lines();
-
-    // Tests
-    let test_input = "vJrwpWtwJgWrhcsFMMfFFhFp";
-    let test_splitter = test_input.split_at(test_input.len() / 2);
+    let input: Vec<String> = fs::read_to_string("./input")
+        .expect("no file")
+        .lines()
+        .map(|l| l.to_string())
+        .collect();
 
     // Part 1
-    let part1: usize = part1_input.map(|a| find_duplicate(a)).map(|a| pri(a)).sum();
+    let part1: usize = input
+        .clone()
+        .iter()
+        .map(|a| {
+            let splitter = a.split_at(a.len() / 2);
+            find_dupes(splitter.0, splitter.1)
+                .chars()
+                .take(1)
+                .next()
+                .unwrap()
+        })
+        .map(|a| pri(a))
+        .sum();
+
     println!("part1: {}", part1);
 
     // Part 2
-    let part2_input = file
-        .split('\n')
-        .filter(|s| !s.is_empty())
-        .collect::<Vec<&str>>();
-
-    let part2: usize = part2_input
+    let part2: usize = input
         .chunks(3)
         .map(|chunk| {
             let dup1 = find_dupes(chunk.get(0).unwrap(), chunk.get(1).unwrap());
@@ -36,16 +43,6 @@ fn main() {
     println!("part 2: {}", part2);
 }
 
-fn find_duplicate(input: &str) -> char {
-    let splitter = input.split_at(input.len() / 2);
-    let duplicate = splitter
-        .0
-        .chars()
-        .find(|a| splitter.1.contains(*a))
-        .unwrap();
-    duplicate
-}
-
 fn find_dupes(a: &str, b: &str) -> String {
     let dupes: String = a.chars().filter(|x| b.contains(*x)).unique().collect();
     dupes
@@ -57,5 +54,3 @@ fn pri(c: char) -> usize {
     let priority = pri.iter().position(|b| b == &c).unwrap() + 1;
     priority
 }
-
-// fn priority(a: char) -> i32 {}
