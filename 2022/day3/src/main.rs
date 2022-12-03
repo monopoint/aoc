6b76b23a5214e6e7de397b8587a79e4fd8343b13
Espen MH <espen@monopoint.net>
2022-12-03 14:57:41 +0100

    refactor

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -25,32 +25,32 @@ fn main() {
         })
         .map(|a| pri(a))
         .sum();
-
     println!("part1: {}", part1);
 
     // Part 2
     let part2: usize = input
         .chunks(3)
         .map(|chunk| {
-            let dup1 = find_dupes(chunk.get(0).unwrap(), chunk.get(1).unwrap());
-            let dup2 = find_dupes(chunk.get(2).unwrap(), &dup1);
-            let c = dup2.chars().take(1).next().unwrap();
-            c
+            find_dupes(
+                chunk.get(0).unwrap(),
+                &find_dupes(chunk.get(1).unwrap(), chunk.get(2).unwrap()),
+            )
+            .chars()
+            .take(1)
+            .next()
+            .unwrap()
         })
         .map(pri)
         .sum();
-
     println!("part 2: {}", part2);
 }
 
 fn find_dupes(a: &str, b: &str) -> String {
-    let dupes: String = a.chars().filter(|x| b.contains(*x)).unique().collect();
-    dupes
+    a.chars().filter(|x| b.contains(*x)).unique().collect()
 }
 
 fn pri(c: char) -> usize {
     let mut pri: Vec<char> = (b'a'..=b'z').map(|a| a as char).collect();
     pri.extend((b'A'..=b'Z').map(|a| a as char).collect::<Vec<char>>());
-    let priority = pri.iter().position(|b| b == &c).unwrap() + 1;
-    priority
+    pri.iter().position(|b| b == &c).unwrap() + 1
 }
